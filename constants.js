431b33215c2a736d7cd82356e0318b65d9994474
Glitch (glitch-hello-website) <none>
2024-12-09 16:52:26 +0000

    🍟🐧 Checkpoint ./index.html:72380599/458 ./collide.js:72380599/646 ./constants.js:72380599/4

--- constants.js
+++ constants.js
@@ -1,5 +1,5 @@
 const BOX_SPEED = 1
 const BOX_NOT_SELECTED_COLOR = 'blue';
 const BOX_SELECTED_COLOR = 'yellow';
-const TORPEDO_SPEED = 1;
-const TORPEDO_MARGIN = 1;
+const TORPEDO_SPEED = 3;
+const TORPEDO_MARGIN = 2;
