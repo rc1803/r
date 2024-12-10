94a69bb85b5ce6a6ba61b54ee595244b931cc2e1
Glitch (glitch-hello-website) <none>
2024-12-10 16:42:55 +0000

    🍙🎨 Checkpoint ./constants.js:72380599/2 ./index.html:72380599/279 ./collide.js:72380599/1503

--- collide.js
+++ collide.js
@@ -21,3 +21,29 @@ AFRAME.registerComponent('collide', {
  },
 
 });
+
+AFRAME.registerComponent('teleport', {
+
+  init: function () {
+    this.el.addEventListener('obbcollisionstarted', this.boxClicked.bind(this));
+  },
+/*
+  boxClicked: function() {
+    let position = this.el.getAttribute('position')
+    position.y = (Math.random())*9+1
+    position.x = (Math.random()-0.5) * 20
+    position.z = (Math.random()*8)-10
+    this.el.setAttribute('position', position)
+  }*/
+  boxClicked: function() {
+    
+   let box = document.getElementById('box');
+   let position = box.getAttribute('position');
+   position.x = Math.random() * 10 * ((Math.random() < 0.5) ? -1 : 1);
+   position.y = Math.random() * 9 + 1;  // Minimum y-value is 1 so it's above ground
+   position.z = -(Math.random() * 8 + 2);
+   box.setAttribute('position', position);
+   this.el.setAttribute('visible', false);
+  }
+
+});
